13b1abcdab9b5525961d3bac5734fdc3572deb47
leroy_jenkem <olli.lindeman@gmail.com>
2022-05-18 21:44:16 +0300

    YESSS
    fullscreen webgl bg

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Curtains pixelRatio={Math.min(1.5, window.devicePixelRatio)}>
+    <Curtains pixelRatio={Math.min(1.55, window.devicePixelRatio)}>
       <AppRouter />
     </Curtains>
   </React.StrictMode>
